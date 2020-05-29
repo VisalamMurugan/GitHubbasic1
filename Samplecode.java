4a0ab61c1c34e839d280457136118529c0a224fc
VisalamMurugan <visalam.murugan@gmail.com>
2020-05-29 10:07:44 +0530

    f2 merge

--- Samplecode.java
+++ Samplecode.java
@@ -1,7 +1,7 @@
 public static void main()
 {
 
-system.out.println("sample data to be validated - feature 2 and 1");
-
+system.out.println("sample data to be validated - f1 2 and 1");
+system.out.println("branch f2");
  
 }
